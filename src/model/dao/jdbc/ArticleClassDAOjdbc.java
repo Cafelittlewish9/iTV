0e5c8a5cfefb08abed02e5371e7e9463327797b6
Shekx <shichering@gmail.com>
2015-09-12 14:28:47 +0800

    build General Constant and ArticleClassDAO

--- src/model/dao/jdbc/ArticleClassDAOjdbc.java
+++ src/model/dao/jdbc/ArticleClassDAOjdbc.java
@@ -10,12 +10,9 @@ import java.util.List;
 
 import model.dao.ArticleClassDAO;
 import model.vo.ArticleClassVO;
+import util.GC;
 
 public class ArticleClassDAOjdbc implements ArticleClassDAO {
-	private final String URL = "jdbc:sqlserver://y56pcc16br.database.windows.net:1433;database=iTV";
-	private final String USERNAME = "iTVSoCool";//iTVSoCool@y56pcc16br
-	private final String PASSWORD = "iTVisgood911";	
-	
 	
 	private static final String SELECT_ALL =
 			"SELECT subclassNo,subclassName,className FROM articleclass";
@@ -23,7 +20,7 @@ public class ArticleClassDAOjdbc implements ArticleClassDAO {
 	public List<ArticleClassVO> selectAll(){
 		ArticleClassVO acvo;
 		List<ArticleClassVO> acvos=new ArrayList<ArticleClassVO>();
-		try(Connection conn=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		try(Connection conn=DriverManager.getConnection(GC.URL, GC.USERNAME, GC.PASSWORD);
 			PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL);) {
 			ResultSet rs=pstmt.executeQuery();		
 			while (rs.next()) {
@@ -44,7 +41,7 @@ public class ArticleClassDAOjdbc implements ArticleClassDAO {
 	@Override
 	public ArticleClassVO select(String subclassNo){
 		ArticleClassVO acvo=null;
-		try (Connection conn=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		try (Connection conn=DriverManager.getConnection(GC.URL, GC.USERNAME, GC.PASSWORD);
 				PreparedStatement pstmt = conn.prepareStatement(SELECT);){			
 			pstmt.setString(1, subclassNo);
 			ResultSet rs=pstmt.executeQuery();		
@@ -64,7 +61,7 @@ public class ArticleClassDAOjdbc implements ArticleClassDAO {
 	@Override
 	public boolean insert(ArticleClassVO articleClass){
 		boolean result=false;		
-		try (Connection conn=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		try (Connection conn=DriverManager.getConnection(GC.URL, GC.USERNAME, GC.PASSWORD);
 				PreparedStatement pstmt = conn.prepareStatement(INSERT);) {
 			pstmt.setString(1,articleClass.getSubclassNo());
 			pstmt.setString(2,articleClass.getSubclassName());
@@ -83,7 +80,7 @@ public class ArticleClassDAOjdbc implements ArticleClassDAO {
 	@Override
 	public boolean update(ArticleClassVO articleClass) {
 		boolean result=false;
-		try (Connection conn=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		try (Connection conn=DriverManager.getConnection(GC.URL, GC.USERNAME, GC.PASSWORD);
 				PreparedStatement pstmt = conn.prepareStatement(UPDATE);){
 			if(articleClass!=null){
 				pstmt.setString(1,articleClass.getSubclassName());
@@ -104,7 +101,7 @@ public class ArticleClassDAOjdbc implements ArticleClassDAO {
 	@Override
 	public boolean delete(String subclassNo) {
 		boolean result=false;
-		try(Connection conn=DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		try(Connection conn=DriverManager.getConnection(GC.URL, GC.USERNAME, GC.PASSWORD);
 				PreparedStatement pstmt = conn.prepareStatement(DELETE);) {
 			pstmt.setString(1, subclassNo);
 			int updateCount = pstmt.executeUpdate();
---
+++ src/util/GC.java
@@ -0,0 +1,9 @@
+package util;
+
+//GC stands for General Constant
+public class GC {
+	public static final String URL = "jdbc:sqlserver://xase7q8rgb.database.windows.net:1433;database=iTV";
+	public static final String USERNAME = "iTVSoCool";
+	public static final String PASSWORD = "iTVisgood911";	
+
+}
