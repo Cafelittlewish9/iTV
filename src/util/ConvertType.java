aefe79177ead69722a454088de86dfd9c7ed7eef
Cafelittlewish9 <cafelittlewish9@gmail.com>
2015-09-14 10:20:29 +0800

    新增轉換當地時間

--- src/util/ConvertType.java
+++ src/util/ConvertType.java
@@ -1,6 +1,10 @@
 package util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.TimeZone;
 
 import model.dao.LoginDAO;
@@ -42,35 +46,46 @@ public class ConvertType {
 		return result;
 	}
 
-	public static String convertToLocalTime(java.util.Date whichYouWantToConvert, String whereIsUser) {
+	public static String convertToLocalTime(java.util.Date whichYouWantToConvert) {
 		String result = null;
+		System.out.println(whichYouWantToConvert);
 		if (whichYouWantToConvert != null) {
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-			sdf.setTimeZone(TimeZone.getTimeZone(whereIsUser));
-			result = sdf.format(whichYouWantToConvert);
+			long a = whichYouWantToConvert.getTime();
+			long b = ZonedDateTime.now().getOffset().getTotalSeconds()*1000;
+			java.util.Date temp = new java.util.Date(a + b);
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			result = sdf.format(temp);
 		}
 		return result;
 	}
 
 	public static void main(String[] args) {
-//		 System.out.println(TimeZone.getDefault());
-//		 String[] temp = TimeZone.getAvailableIDs();
-//		 for (String a : temp) {
-//		 System.out.println(a);
-//		 }
-//		Set<String> temp2 = ZoneId.getAvailableZoneIds();
-//		for (String b : temp2) {
-//			if (b.contains("Asia")) {
-//				b = b.substring(4);
-//				System.out.println(b);
-//			}
-//		}
-//		String time = ConvertType.convertToLocalTime(new java.util.Date(System.currentTimeMillis()), "Asia/Taipei");
-//		System.out.println(time);
-		
+		// System.out.println(TimeZone.getDefault());
+		// String[] temp = TimeZone.getAvailableIDs();
+		// for (String a : temp) {
+		// System.out.println(a);
+		// }
+		// Set<String> temp2 = ZoneId.getAvailableZoneIds();
+		// for (String b : temp2) {
+		// if (b.contains("Asia")) {
+		// b = b.substring(4);
+		// System.out.println(b);
+		// }
+		// }
+		// String time = ConvertType.convertToLocalTime(new
+		// java.util.Date(System.currentTimeMillis()), "Asia/Taipei");
+		// System.out.println(time);
+
 		LoginDAO dao = new LoginDAOjdbc();
-		LoginVO bean = dao.select("Pikachu");
-		String time2 = ConvertType.convertToLocalTime(bean.getLoginTime(), "Asia/Taipei");
-		System.out.println(time2);
+		List<LoginVO> bean = dao.selectAll("Pikachu");
+		for (LoginVO a : bean) {
+			String time2 = ConvertType.convertToLocalTime(a.getLoginTime());
+			System.out.println(time2);
+		}
+		// System.out.println(ZonedDateTime.now().getOffset().getId());
+		// System.out.println(ZonedDateTime.now().getOffset().getTotalSeconds());
+		// System.out.println(ZonedDateTime.now().getOffset().getRules());
+		// System.out.println(ZonedDateTime.now().getOffset().toString());
+		// System.out.println(60*60*8);
 	}
 }
